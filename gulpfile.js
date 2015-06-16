c9d4673479324724c1a1f32a79455b6d77d64658
Splendorr <simolinic@gmail.com>
2015-06-15 21:54:40 -0400

    Set up a bunch of gulp helpers.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var sass = require('gulp-ruby-sass');
+// var sass = require('gulp-ruby-sass');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var csslint = require('gulp-csslint');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+var axis = require('axis');
+var jeet = require ('jeet');
+var rupture = require ('rupture');
+var React = require('react');
+// var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
 
-gulp.task("default", function () {
-  console.log("gulp gulp gulp");
+gulp.task('default', ['compile:js', 'compile:css', 'browser-sync'], function () {
+  gulp.watch(["./src/js/**/*.js"], ["compile:js"]).on('change', reload);
+  gulp.watch(["./src/css/**/*.styl"], ["compile:css"]).on('change', reload);
+});
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: "./public"
+  });
 });
 
 gulp.task("jshint", function () {
@@ -26,20 +42,24 @@ gulp.task("compile:js", ["jshint"], function () {
 
 gulp.task("compile:css", function () {
 
-  return sass('./src/scss/') 
-    .on('error', function (err) {
-      console.error('Error!', err.message);
-   })
+  gulp.src('./src/css/**/*.styl')
+    // .pipe(stylus())
+    .pipe(stylus({ use: [axis(), jeet(), rupture()], compress: false, 'include css': true }))
     .pipe(csslint())
     .pipe(csslint.reporter())
     .pipe(gulp.dest('./public/assets/css'));
 
-  // gulp.src(["./src/scss/*.scss"])
-  //   .pipe(sass())
-  //   .pipe(gulp.dest("./public/assets/css"));
+  // return sass('./src/scss/') 
+  //   .on('error', function (err) {
+  //     console.error('Error!', err.message);
+  //  })
+  //   .pipe(csslint())
+  //   .pipe(csslint.reporter())
+  //   .pipe(gulp.dest('./public/assets/css'));
+
 });
 
 gulp.task("watch", ["compile:css", "compile:js"], function () {
   gulp.watch(["./src/js/**/*.js"], ["compile:js"]);
-  gulp.watch(["./src/scss/**/*.scss"], ["compile:css"]);
+  gulp.watch(["./src/css/**/*.styl"], ["compile:css"]);
 });
